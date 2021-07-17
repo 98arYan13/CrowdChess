68ad13552df9d711678804eccf77c5463cd04abf
Majid Amin <ikoiran@gmail.com>
2021-07-17 22:27:03 +0430

    drawin recommended move availible

--- src/app.py
+++ src/app.py
@@ -16,57 +16,6 @@ import json
 # create web app instance
 app = Flask(__name__)
 
-# just for white first move (computer)
-# probe book move
-def probe_book(pgn):
-    # open book file
-    with open('./engine/book.txt') as f:
-        # read book games
-        book = f.read()
-
-        # init board        
-        board = chess.Board()
-
-        # define response moves
-        response_moves = []
-
-        # loop over book lines
-        for line in book.split('\n')[0:-1]:
-            # define variation
-            variation = []
-            
-            # loop over line moves
-            for move in line.split():
-                variation.append(chess.Move.from_uci(move))
-            
-            # parse variation to SAN
-            san = board.variation_san(variation)
-            
-            # match book line line
-            if pgn in san:
-                try:
-                    # black move
-                    if san.split(pgn)[-1].split()[0][0].isdigit():
-                        response_moves.append(san.split(pgn)[-1].split()[1])
-                    
-                    # white move
-                    else:
-                        response_moves.append(san.split(pgn)[-1].split()[0])
-                
-                except:
-                    pass
-            
-            # engine makes first move
-            if pgn == '':
-                response_moves.append(san.split('1. ')[-1].split()[0])
-
-        # return random response move
-        if len(response_moves):
-            print('BOOK MOVE:', random.choice(response_moves))
-            return random.choice(response_moves)
-        
-        else:
-            return 0
 
 # root(index) route
 @app.route('/')
@@ -86,13 +35,6 @@ def max_legal_moves():
     except: # if new game
         return {'max_legal_moves': '20'}
 
-
-# Recommended moves
-app.route('/recommend_moves', methods=['POST'])
-def recommend_moves():
-    make_move()
-    return {'pv'}
-
 # make move API
 @app.route('/make_move', methods=['POST'])
 def make_move():
@@ -110,87 +52,164 @@ def make_move():
         for move in game.mainline_moves():
             # make move on chess board
             board.push(move)
+    
+    except:
+        # extract fen string from HTTP POST request body
+        fen = request.form.get('fen')
+        board = chess.Board(fen)
         
-        # create chess engine instance
-        engine = chess.engine.SimpleEngine.popen_uci(
-            './engine/stockfish_13/stockfish_13_win_x64_bmi2.exe')
-        
-        # extract fixed depth value
-        fixed_depth = request.form.get('fixed_depth')
+    # create chess engine instance
+    engine = chess.engine.SimpleEngine.popen_uci(
+        './engine/stockfish_13/stockfish_13_win_x64_bmi2.exe')
+    
+    # extract fixed depth value
+    fixed_depth = request.form.get('fixed_depth')
 
-        # extract move time value
-        move_time = request.form.get('move_time')
-        
-        # if move time is available
-        if move_time != '0':
-            if move_time == 'instant':
-                try:
-                    # search for best move instantly
-                    info = engine.analyse(board, chess.engine.Limit(time=0.1))
-                except:
-                    info = {}
-            else:
-                try:
-                    # search for best move with fixed move time
-                    info = engine.analyse(board, chess.engine.Limit(time=int(move_time)))
-                except:
-                    info = {}
-
-        # if fixed depth is available
-        if fixed_depth != '0':
+    # extract move time value
+    move_time = request.form.get('move_time')
+    
+    # if move time is available
+    if move_time != '0':
+        if move_time == 'instant':
             try:
                 # search for best move instantly
-                info = engine.analyse(board, chess.engine.Limit(depth=int(fixed_depth)))
+                info = engine.analyse(board, chess.engine.Limit(time=0.1))
             except:
                 info = {}
-        
-        # terminate engine process
-        engine.quit()
-        
-        try:
-            # extract best move from PV
-            best_move = info['pv'][0]
-
-            # update internal python chess board state
-            board.push(best_move)
-            
-            # get best score
+        else:
             try:
-                score = -int(str(info['score'])) / 100
-            
+                # search for best move with fixed move time
+                info = engine.analyse(board, chess.engine.Limit(time=int(move_time)))
             except:
-                score = str(info['score'])
-                
-                # inverse score
-                if '+' in score:
-                    score = score.replace('+', '-')
-                
-                elif '-' in score:
-                    score = score.replace('-', '+')
-            
-            return {
-                'fen': board.fen(),
-                'best_move': str(best_move),
-                'score': score,
-                'depth': info['depth'],
-                'pv': ' '.join([str(move) for move in info['pv']]),
-                'nodes': info['nodes'],
-                'time': info['time'],
-            }
+                info = {}
+
+    # if fixed depth is available
+    if fixed_depth != '0':
+        try:
+            # search for best move instantly
+            info = engine.analyse(board, chess.engine.Limit(depth=int(fixed_depth)))
+        except:
+            info = {}
+    
+    # terminate engine process
+    engine.quit()
+    
+    try:
+        # extract best move from PV
+        best_move = info['pv'][0]
+
+        # update internal python chess board state
+        board.push(best_move)
+        
+        # get best score
+        try:
+            score = -int(str(info['score'])) / 100
         
         except:
-            return {
-                'fen': board.fen(),
-                'score': '#+1',
-            }
-
+            score = str(info['score'])
+            
+            # inverse score
+            if '+' in score:
+                score = score.replace('+', '-')
+            
+            elif '-' in score:
+                score = score.replace('-', '+')
+        
+        return {
+            'fen': board.fen(),
+            'best_move': str(best_move),
+            'score': score,
+            'depth': info['depth'],
+            'pv': ' '.join([str(move) for move in info['pv']]),
+            'nodes': info['nodes'],
+            'time': info['time'],
+        }
+    
     except:
-        # probe opening book
-        if probe_book(pgn):
-            return {
-                'score': 'book move',
-                'best_move': probe_book(pgn)
-            }
+        return {
+            'fen': board.fen(),
+            'score': '#+1',
+        }
+
+# Recommended moves
+@app.route('/recommend_moves', methods=['POST'])
+def recommend_moves():
+    # set number of multipv lines for recommended moves
+    MULTIPV = 3
+
+    # extract PGN string from HTTP POST request body
+    pgn = request.form.get('pgn')
+    
+    try:
+        # read game moves from PGN
+        game = chess.pgn.read_game(io.StringIO(pgn))
+
+        # init board
+        board = game.board()
+
+        # loop over moves in game
+        for move in game.mainline_moves():
+            # make move on chess board
+            board.push(move)
+    
+    except:
+        # extract fen string from HTTP POST request body
+        fen = request.form.get('fen')
+        board = chess.Board(fen)
+        
+    # create chess engine instance
+    engine = chess.engine.SimpleEngine.popen_uci(
+        './engine/stockfish_13/stockfish_13_win_x64_bmi2.exe')
+    
+    # extract fixed depth value
+    fixed_depth = request.form.get('fixed_depth')
+
+    # extract move time value
+    move_time = request.form.get('move_time')
+    
+    # if move time is available
+    if move_time != '0':
+        if move_time == 'instant':
+            try:
+                # search for best move instantly
+                info = engine.analyse(
+                    board, chess.engine.Limit(time=0.1), multipv=MULTIPV)
+            except:
+                info = {}
+        else:
+            try:
+                # search for best move with fixed move time
+                info = engine.analyse(
+                    board, chess.engine.Limit(time=int(move_time)), multipv=MULTIPV)
+            except:
+                info = {}
+
+    # if fixed depth is available
+    if fixed_depth != '0':
+        try:
+            # search for best move instantly
+            info = engine.analyse(
+                board, chess.engine.Limit(depth=int(fixed_depth)), multipv=MULTIPV)
+        except:
+            info = {}
+    
+    # terminate engine process
+    engine.quit()
+    
+    print('best move is: ', info[0]['pv'][0])
+
+    print("\ninfo:\n", info)
+    random.shuffle(info)
+
+    print("randomised first pv: ", info[0]['pv'][0])
+    print("\ninfo_shuffled:\n", info)
+        
+    return {
+        'fen': board.fen(),
+        'score_array': [str(i['score']) for i in info],
+        'depth_array': [str(i['depth']) for i in info],
+        'pv_array': [' '.join([str(move) for move in i['pv']]) for i in info]
+    }
 
 """
 @app.route('/analytics')
