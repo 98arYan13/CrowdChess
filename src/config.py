d49b33e3155ee3b0839a42c6033a59a26c6d5778
Majid Amin <ikoiran@gmail.com>
2021-07-26 21:43:18 +0430

    config.py keep safe

--- src/config.py
+++
@@ -1,8 +0,0 @@
-# this is for admin login
-# change secret_key & password
-
-USERNAME = 'majid'
-PASS = 'password'
-SECRET_KEY = "b'\x9e\xaa\x9d\xea\x80\x10Jk\xa2\xc6\x13r\xc0\x18\xdeZ'" # TODO: use this command: py -c 'import os; print(os.urandom(16))'
-
-DEBUG = False
