65ab2bcce5e0efc459161056bade575d6d9b2354
Majid Amin <ikoiran@gmail.com>
2021-08-05 18:15:47 +0430

    no problem

--- src/__init__.py
+++ src/__init__.py
@@ -31,7 +31,6 @@ def create_app(debug=False):
     from models import User
     @login_manager.user_loader
     def load_user(user_id): #reload user object from the user ID stored in the session
-        # since the user_id is just the primary key of our user table, use it in the query for the user
         return User.query.get(int(user_id))
     
     # blueprint for auth routes
--- src/auth.py
+++ src/auth.py
@@ -41,7 +41,8 @@ def signup(): # define the sign up function
             flash('Email address already exists')
             return redirect(url_for('auth.signup'))
         # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-        new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256')) #
+        new_user = User(email=email, name=name, password=generate_password_hash(
+            password, method='sha256'))
         # add the new user to the database
         db.session.add(new_user)
         db.session.commit()
