06655f88462551feb100af639dcbd8edb401a4a2
Majid Amin <ikoiran@gmail.com>
2021-10-03 20:52:12 +0330

    change size of chess_board and canvas according to display

--- src/static/js/draw-arrows.js
+++ src/static/js/draw-arrows.js
@@ -1,31 +1,35 @@
 // source: https://stackoverflow.com/questions/25527902/drawing-arrows-on-a-chess-board-in-javascript
 function drawArrow(arrow, color) {
+
+    // get size of canvas according to chess_board size
+    canvasWidth = $('#chess_board').width();
+
     //create a canvas over the chessboard
     if ($("#canvas").length === 0) {
-        var canvas = '<canvas id="canvas" class="canvas" width="396" height="396"><\/canvas>';
+        var canvas = '<canvas id="canvas" class="canvas" width=' + canvasWidth + ' height=' + canvasWidth + '><\/canvas>';
         $("#chess_board").append(canvas);
     }
     
     //variables to be used when creating the arrow
     var ctx = document.getElementById("canvas").getContext('2d')
-    var headlen = 5;
+    var headlen = Math.round(0.0125 * canvasWidth);
 
     //arrow = "h1a8"
     
     // coordinations of tip and tale of arrow by file letter to ascii, and rank number
     // oriantation = 'white'
     if(board.orientation() == 'white') {
-        var fromx = ((arrow.charCodeAt(0) - 97) * 50 + 25);
-        var tox = ((arrow.charCodeAt(2) - 97) * 50 + 25);
-        var fromy = 396 - (parseInt(arrow[1]) * 49 - 25);
-        var toy = 396 - (parseInt(arrow[3]) * 49 - 25);
+        var fromx = Math.round((arrow.charCodeAt(0) - 97) * (canvasWidth / 8) + (canvasWidth / 16));
+        var tox = Math.round((arrow.charCodeAt(2) - 97) * (canvasWidth / 8) + (canvasWidth / 16));
+        var fromy = Math.round(canvasWidth - (parseInt(arrow[1]) * (canvasWidth / 8) - (canvasWidth / 16)));
+        var toy = Math.round(canvasWidth - (parseInt(arrow[3]) * (canvasWidth / 8) - (canvasWidth / 16)));
     }
     // oriantation = 'black'
     if(board.orientation() == 'black') {
-        var fromx = 396 - ((arrow.charCodeAt(0) - 97) * 50 + 25);
-        var tox = 396 - ((arrow.charCodeAt(2) - 97) * 50 + 25);
-        var fromy = (parseInt(arrow[1]) * 49 - 25);
-        var toy = (parseInt(arrow[3]) * 49 - 25);
+        var fromx = Math.round(canvasWidth - ((arrow.charCodeAt(0) - 97) * (canvasWidth / 8) + (canvasWidth / 16)));
+        var tox = Math.round(canvasWidth - ((arrow.charCodeAt(2) - 97) * (canvasWidth / 8) + (canvasWidth / 16)));
+        var fromy = Math.round(parseInt(arrow[1]) * (canvasWidth / 8) - (canvasWidth / 16));
+        var toy = Math.round(parseInt(arrow[3]) * (canvasWidth / 8) - (canvasWidth / 16));
     }
 
     var angle = Math.atan2(toy-fromy,tox-fromx);
@@ -35,7 +39,7 @@ function drawArrow(arrow, color) {
     ctx.moveTo(fromx, fromy);
     ctx.lineTo(tox, toy);
     ctx.strokeStyle = color;
-    ctx.lineWidth = 15;
+    ctx.lineWidth = Math.round(0.04 * canvasWidth);
     ctx.stroke();
 
     //starting a new path from the head of the arrow to one of the sides of the point
@@ -52,7 +56,7 @@ function drawArrow(arrow, color) {
 
     //draws the paths created above
     ctx.strokeStyle = color;
-    ctx.lineWidth = 15;
+    ctx.lineWidth = Math.round(0.04 * canvasWidth);
     ctx.stroke();
     ctx.fillStyle = color;
     ctx.fill();
